2c103e4380a5b24d72414e95575f4fba945fbe43
LouisHaquette <louis@alphee.fr>
2015-01-08 21:48:58 +0100

    Create index.php

---
+++ view/home/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Louis Haquette</title>
+		<link href="view/css/main.css" rel="stylesheet" />
+		<link href="view/asset/logo/favicon.png" rel="shortcut icon" 
+		type="image/x-icon">
+	</head>
+
+	<body>
+
+		<div id="wrap">
+
+			<?php
+				include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainHeader.php');
+			?>
+
+			<div id="main">
+
+				<?php
+					include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainAside.php');
+				?>
+
+			
+				<section id="mainSection">
+
+				</section>
+
+			</div>
+			
+		</div>
+
+		<?php
+			include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainFooter.php');
+		?>
+
+	</body>
+</html>
