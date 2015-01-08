e13c6b6316222bb6992345030314e8f4c2d5fa71
LouisHaquette <louis@alphee.fr>
2015-01-08 21:42:56 +0100

    Create index.php

---
+++ controller/home/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+include_once('modele/blog/get_billets.php');
+$billets = get_billets(0, 5);
+
+foreach($billets as $cle => $billet)
+{
+    $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
+    $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu']));
+}
+*/
+
+include_once('/opt/lampp/htdocs/projects/LouisHaquette/view/home/index.php');
