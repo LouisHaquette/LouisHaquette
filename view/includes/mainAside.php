7b85555edbc3a29051fab87f71b4191573485c79
LouisHaquette <louis@alphee.fr>
2015-01-08 21:49:59 +0100

    Create mainAside.php

---
+++ view/includes/mainAside.php
@@ -0,0 +1,15 @@
+<!--mainAside-->
+<aside id="networks">
+	<p>Suivez-moi sur : </p>
+	<nav>
+	<!--socials networks : -->
+		<ul id="linksNetworks">
+			<li><a href="https://www.facebook.com/LouisHaquette" title="Facebook">Facebook</a></li>
+			<li><a href="https://twitter.com/louislepro62" title="Twitter">Twitter</a></li>
+			<li><a href="https://plus.google.com/u/0/101465254157336425309/posts" title="google+">Google+</a></li>
+			<li><a href="http://instagram.com/louislepro62" title="Instagram">Instagram</a></li>
+			<li><a href="https://www.flickr.com/" title="Flickr">Flickr</a></li>
+			<li><a href="https://www.youtube.com/channel/UCFiryw63ifzDQNabz5xp-CA/" title="Youtube">Youtube</a></li>
+		</ul>
+	</nav>
+</aside>
