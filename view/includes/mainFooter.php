399466b3d773c0659a9d41a994daff726601e5bb
LouisHaquette <louis@alphee.fr>
2015-01-08 21:52:01 +0100

    Create mainFooter.php

---
+++ view/includes/mainFooter.php
@@ -0,0 +1,14 @@
+<!--mainFooter-->
+<footer id="mainFooter">
+
+	<object type="image/svg+xml" data="view/asset/logo/GNU_Linux_Tux_coffee_logo_Starbucks_Tux.svg" 
+	width="64" height="64" id="logoFoot">
+    	Logo footer SVG
+	</object>
+
+	<p>
+		<a href="mailto:louis@alphee.fr" title="me contacter">Louis Haquette</a> 
+		2014 (<a href="http://creativecommons.org/licenses/by-sa/4.0/" 
+		title="creative commons CC BY-SA 4.0">CC BY-SA</a>)
+	</p>
+</footer>
