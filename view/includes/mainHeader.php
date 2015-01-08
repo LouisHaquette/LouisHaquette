383e449046eab03f02e5407556af74e72ac44eba
LouisHaquette <louis@alphee.fr>
2015-01-08 21:51:02 +0100

    Create mainHeader.php

---
+++ view/includes/mainHeader.php
@@ -0,0 +1,18 @@
+<!--mainHeader-->
+<header id="mainHeader">
+	
+	<object type="image/svg+xml" data="view/asset/logo/GNU_Linux_Tux_coffee_logo_Starbucks_Tux.svg" 
+	width="96" height="96" id="logoHead">
+    	Logo header SVG
+	</object>
+
+	<p>Louis Haquette</p>
+
+	<nav>
+		<ul>
+			<li><a href="#" title="#">HOME</a></li>
+			<li><a href="#" title="#">Le site</a></li>
+			<li><a href="#" title="#">Divers</a></li>
+		</ul>
+	</nav>
+</header>
