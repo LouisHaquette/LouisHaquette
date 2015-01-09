6df0b33e27ecd38ed6e9b49ec6991858a73915f1
LouisHaquette <louis@alphee.fr>
2015-01-09 19:42:49 +0100

    Create lesite.php

---
+++ lesite.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once('model/sql_connection.php');
+
+if (!isset($_GET['section']) OR $_GET['section'] == 'index')
+{
+    include_once('/opt/lampp/htdocs/projects/LouisHaquette/controller/lesite/index.php');
+}
