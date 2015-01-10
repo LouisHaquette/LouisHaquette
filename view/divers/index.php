e192c54775f00deb8a315ef5780158bf10c3a5c9
LouisHaquette <louis@alphee.fr>
2015-01-10 19:44:19 +0100

    Create index.php

---
+++ view/divers/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+	<head lang="fr">
+		<meta charset="utf-8" />
+		<title>Divers</title>
+		<link href="view/css/main.css" rel="stylesheet" />
+		<link href="view/asset/logo/favicon.png" rel="shortcut icon" 
+		type="image/x-icon" />
+	</head>
+
+	<body>
+
+		<div id="wrap">
+
+			<?php
+				include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainHeader.php');
+			?>
+
+			<div id="main">
+			
+				<section id="diversSection" span="bodySection">
+					<h2>Divers</h2>
+					<h3>Quelques liens :</h3>
+					
+					<ul>
+						<li><strong>Projet GNU et logiciels libres</strong></li>
+						<ul>
+							<li><a href="https://www.fsf.org/">La Free Software Foundation (en Anglais)</a></li>
+							<li><a href="https://www.gnu.org/home.fr.html">Le projet GNU</a></li>
+							<li><a href="http://www.framasoft.net/">Framasoft</a></li>
+							<li><a href="http://www.april.org/">l'April</a></li>
+							<li><a href="https://www.mozilla.org/fr/">la Mozilla foundation</a></li>
+							<li><a href="http://ubuntu-fr.org/">Ubuntu</a></li>
+							<li><a href="http://openclassrooms.com/courses/reprenez-le-controle-a-l-aide-de-linux">Tutoriel sur GNU/Linux</a></li>
+						</ul>
+						
+						<li><strong>Les amis</strong></li>
+						<ul>
+							<li><a href="http://www.phenomen.be/">L'équipe de Phenomen, chez qui j'ai qui j'ai fais mon stage de 4ème :)</a></li>
+							<li><a href="https://www.foxi.be/">Leur site foxi.be</a></li>
+						</ul>
+					
+						<li><strong>Tutos sur le developpement Web</strong></li>
+						<ul>
+							<li><a href="http://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3">HTML5/CSS3</a></li>
+							<li><a href="http://openclassrooms.com/courses/concevez-votre-site-web-avec-php-et-mysql">PHP + MySQL</a></li>
+							<li><a href="http://openclassrooms.com/courses/dynamisez-vos-sites-web-avec-javascript">JavaScript</a></li>
+						</ul>
+						
+						<li><strong>Sites et vidéos sur les sciences</strong></li>
+						<ul>
+							<li><a href="https://www.youtube.com/user/epenser1">e-penser (sur Youtube)</a></li>
+							<li><a href="https://www.youtube.com/user/experimentboyTV">experimentBoy (sur Youtube)</a></li>
+							<li><a href="http://micmaths.com/">Micmath</a></li>
+							<li><a href="http://www.futura-sciences.com/">Futura-sciences</a></li>
+						</ul>
+						
+						<li><strong>Autres liens</strong></li>
+						<ul>
+							<li>Rien pour l'instant :P</li>
+						</ul>
+						
+					</ul>
+					
+				</section>
+
+				<?php
+					include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainArrow.php');
+				?>
+
+			</div>
+			
+		</div>
+
+		<?php
+			include('/opt/lampp/htdocs/projects/LouisHaquette/view/includes/mainFooter.php');
+		?>
+
+	</body>
+</html>
